492648d2f84c5184185831aa59d10be0d14fa4ed
qytayh <13255238145@163.com>
2021-09-21 22:17:22 +0800

    first commit

--- source/koa.js
+++ source/koa.js
@@ -18,6 +18,7 @@ class Koa {
     use(callback){
         this.callback = callback
     }
+    // 构建上下⽂, 把res和req都挂载到ctx之上，并且在ctx.req和ctx.request.req同时保存
     createContext(req,res){
         const ctx = Object.create(context)
         ctx.request = Object.create(request)
