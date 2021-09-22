04b8900efa08a7efe4de37fd81d5cee9a3be734c
qytayh <13255238145@163.com>
2021-09-22 22:15:46 +0800

    完成compose中间件以及router

--- index.js
+++ index.js
@@ -11,10 +11,38 @@
 const Koa = require('./source/koa')
 const app = new Koa()
 
-app.use((req,res)=>{
-    res.writeHead(200)
-    res.end('hello world!')
-})
-app.listen(300,()=>{
+const Router = require('./source/router')
+const router = new Router();
+
+
+router.get('/index', async ctx => {
+    console.log('index,xx')
+    ctx.body = 'index page';
+});
+router.get('/post', async ctx => { ctx.body = 'post page'; });
+router.get('/list', async ctx => { ctx.body = 'list page'; });
+router.post('/index', async ctx => { ctx.body = 'post page'; });
+
+
+app.use(router.routes())
+
+
+// const delay = () => new Promise(resolve => setTimeout(() => resolve(), 2000));
+
+// app.use(async (ctx, next) => {
+//     ctx.body = "1";
+//     await next();
+//     ctx.body += "5";
+// });
+// app.use(async (ctx, next) => {
+//     ctx.body += "2";
+//     await delay();
+//     await next();
+//     ctx.body += "4";
+// });
+// app.use(async (ctx, next) => {
+//     ctx.body += "3";
+// });
+app.listen(3000, () => {
     console.log('server start at 3000')
 })
--- source/koa.js
+++ source/koa.js
@@ -4,19 +4,31 @@ const request = require('./request')
 const response = require('./response')
 
 class Koa {
+    constructor(){
+        this.middlewares = []
+    }
+
     listen(...args){
-        const server = http.createServer((req,res)=>{
+        const server = http.createServer(async (req,res)=>{
             // this.callback(req,res)
             // 创建上下文
             const ctx = this.createContext(req,res)
-            this.callback(ctx)
+            // this.callback(ctx)
+
+            //中间件合成
+            const fn = this.compose(this.middlewares)
+            await fn(ctx)
+
             // 相应
             res.end(ctx.body)
         })
         server.listen(...args)
     }
-    use(callback){
-        this.callback = callback
+    // use(callback){
+    //     this.callback = callback
+    // }
+    use(middleware){
+        this.middlewares.push(middleware)
     }
     // 构建上下⽂, 把res和req都挂载到ctx之上，并且在ctx.req和ctx.request.req同时保存
     createContext(req,res){
@@ -26,6 +38,24 @@ class Koa {
         ctx.req = ctx.request.req = req
         ctx.res = ctx.response.res = res
         return ctx
+    }
+    // 合成函数
+    compose(middlewares){
+        return function(ctx){ // 传入上下文
+            return dispatch(0)
+            function dispatch(i){
+                let fn = middlewares[i]
+                if(!fn){
+                    return Promise.resolve()
+                }
+                return Promise.resolve(
+                    fn(ctx,function next(){
+                        // promise完成后，再执⾏下⼀个
+                        return dispatch(i+1)
+                    })
+                )
+            }
+        }
     }
 }
 
